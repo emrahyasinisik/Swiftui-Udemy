5c7f61e1b7e63225c5ec47dbc2394c58303cb51e
Emrah Yasin IŞIK <112324611+emrahyasinisik@users.noreply.github.com>
2025-07-18 16:10:59 +0300

    Application Start

---
+++ iOS Udemy/ResultScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ResultScreen.swift
+//  iOS Udemy
+//
+//  Created by Emrah IŞIK on 18.07.2025.
+//
+
+import SwiftUI
+
+struct ResultScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultScreen()
+}
