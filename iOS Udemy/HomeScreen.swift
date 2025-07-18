a432ffbf1e6dca9788bf44d5a1d7b3a3f207d033
Emrah Yasin IŞIK <112324611+emrahyasinisik@users.noreply.github.com>
2025-07-18 16:28:41 +0300

    Trial

--- iOS Udemy/HomeScreen.swift
+++ iOS Udemy/HomeScreen.swift
@@ -11,7 +11,7 @@ struct HomeScreen: View {
     var body: some View {
         VStack {
             Button("Başla"){
-                
+                print("Bitti")
             }
         }
     }
